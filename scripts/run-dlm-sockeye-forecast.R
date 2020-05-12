d8004c555bdb68cfbc2e19c8fe27fe2453803f8e
Curry Cunningham <cunninghamcurry@gmail.com>
2020-05-12 13:57:10 -0800

    Init script to run DLM sibling forcast

---
+++ scripts/run-dlm-sockeye-forecast.R
@@ -0,0 +1,15 @@
+#==================================================================================================
+#Project Name: SALMON FORECAST PAPER - Dynamic Linear Sibling Models (DLMs)
+#Creator: Curry James Cunningham, College of Fisheries and Ocean Sciences, UAF
+#Date: 3.10.17
+#
+#Purpose: To generate retrospective forecasts from dynamic linear sibling models
+#
+#
+#==================================================================================================
+#NOTES:
+
+#==================================================================================================
+require(tidyverse)
+require(dplyr)
+require(ggthemes)
