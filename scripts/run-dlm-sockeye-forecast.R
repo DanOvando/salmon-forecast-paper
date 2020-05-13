d23cd0699a089e7636953e6bf19b8f8c1a9ff0e4
Curry Cunningham <cunninghamcurry@gmail.com>
2020-05-12 18:24:21 -0800

    Add control, data loading, and write sections

--- scripts/run-dlm-sockeye-forecast.R
+++ scripts/run-dlm-sockeye-forecast.R
@@ -13,3 +13,34 @@
 require(tidyverse)
 require(dplyr)
 require(ggthemes)
+require(MARSS)
+require(readr)
+require(here)
+
+# Control Section ---------------------------------------------------------
+
+# Controls for 1-step ahead prediction
+years <- 2000:2018
+n.years <- length(years)
+
+stocks <- c('Kvichak','Alagnak','Naknek','Egegik','Ugashik','Igushik','Wood','Nushagak','Togiak')
+n.stocks <- length(stocks) 
+
+# Controls for DLM model
+maxit <- 1e4
+model.type <- 0 #NO log transformation
+start.year <- 1963
+
+# load data ---------------------------------------------------------------
+
+dat <- read.csv(here::here("data", "2019.csv"), stringsAsFactors = FALSE) 
+
+# Source Model Script ------------------------------------------------------
+source(here::here("functions","fit-dlm-model.R"))
+
+
+
+# Write Output -------------------------------------------------------------
+# write_rds(lag_model, path = file.path(results_dir,"lag_model_predictions.rds"))
+
+
